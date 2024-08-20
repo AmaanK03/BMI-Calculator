602f538fc8ca1b6e0d292dac82db3a8dd9d70358
Amaan Khan <amaank3424@gmail.com>
2024-08-20 12:53:03 +0100

    Add files via upload
    Updated version_1.py with corrected BMI calculator logic

--- version_1.py
+++ version_1.py
@@ -11,15 +11,14 @@ bmi = round(calc,2)
 
 print(f'\nYour bmi is {bmi}')
 
-
 if bmi < 18.5:
     print("you are underweight")
-elif bmi == 18.5 or bmi < 25:  # Error: The condition "bmi == 18.5" is redundant because "bmi < 25" already includes it.
+elif 18.5 <= bmi < 25:
     print("you are at a healthy weight")
+elif 25 <= bmi < 30:
+    print("you are overweight")
+else:
+    print("You are obese")
 
-elif bmi == 25 or bmi < 30:  # Error: The condition "bmi == 25" is redundant because "bmi < 30" already includes it.
-    print("you are overweight !")
 
-elif bmi > 30:  # Error: This condition should be "bmi >= 30" to properly include a BMI of 30 in the obese category.
-    print("you are obese !")
 
